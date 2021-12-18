058c1581cb4aaa4abb119693779d9f8a2cd1bf18
gnaD_oahK <trieuthienhkhoa@gmail.com>
2021-12-18 22:37:30 +0200

    finish code

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -1,8 +1,28 @@
 import { Col, Row, Input, Typography, Radio, Select, Tag } from 'antd';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { priorityFilterChange, searchFilterChange, statusFilterChange } from '../../redux/actions';
 
 const { Search } = Input;
 
 export default function Filters() {
+  const [search,setSearch]=useState('');
+  const [filterStatus,setFilterStatus]=useState("All")
+  const [filterPriorities,setFilterPriorities]=useState([])
+  const dispatch= useDispatch();
+  const handleSearchText = e =>{ 
+      setSearch(e.target.value)
+      dispatch(searchFilterChange(e.target.value))
+  }
+  const handleStatusChange = (e)=>{
+    setFilterStatus(e.target.value)
+    dispatch(statusFilterChange(e.target.value))
+  }
+  const handlePrioritiesChange=(value)=>{
+  
+    setFilterPriorities(value)
+    dispatch(priorityFilterChange(value))
+  }
   return (
     <Row justify='center'>
       <Col span={24}>
@@ -11,7 +31,7 @@ export default function Filters() {
         >
           Search
         </Typography.Paragraph>
-        <Search placeholder='input search text' />
+        <Search placeholder='input search text' value={search} onChange={handleSearchText} />
       </Col>
       <Col sm={24}>
         <Typography.Paragraph
@@ -19,7 +39,7 @@ export default function Filters() {
         >
           Filter By Status
         </Typography.Paragraph>
-        <Radio.Group>
+        <Radio.Group value={filterStatus} onChange={handleStatusChange}>
           <Radio value='All'>All</Radio>
           <Radio value='Completed'>Completed</Radio>
           <Radio value='Todo'>To do</Radio>
@@ -36,6 +56,8 @@ export default function Filters() {
           allowClear
           placeholder='Please select'
           style={{ width: '100%' }}
+          value ={filterPriorities}
+          onChange={handlePrioritiesChange}
         >
           <Select.Option value='High' label='High'>
             <Tag color='red'>High</Tag>
